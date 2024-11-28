dd717949682753d0baa6eb840c63eda4fc0cd353
cantoral18 <cantoralmaritza6@gmail.com>
2024-11-28 10:52:35 -0600

    ejercicio 10 numero de caracteres

---
+++ 10. Número de caracteres/ejercicio.js
@@ -0,0 +1,16 @@
+
+// escribe tu respuesta acá
+function numeroDeCaracteres(str, char) {
+    let count = 0;
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === char) {
+            count++;
+        }
+    }
+    
+    return count;
+}
+// código de prueba
+console.log(numeroDeCaracteres("Hola Mundo", "o")) // 2
+console.log(numeroDeCaracteres("MMMMM", "m")) // 0
+console.log(numeroDeCaracteres("eeee", e)) // 4
